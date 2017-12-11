63207cb83d7bb73b1427f936f2114031f5f11c7a
Jim Verheijde <jim.verheijde@hotmail.com>
2017-12-12 00:08:31 +0100

    Add material module and update deps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,21 @@ import { AppComponent } from './app.component';
 import { CalculatorComponent } from './calculator/calculator.component';
 import {ProfitcalculatorService} from './services/profitcalculator.service';
 import { DonateComponent } from './donate/donate.component';
+import {MaterialModule} from './material/material.module';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CalculatorComponent,
-    DonateComponent
+    DonateComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MaterialModule
   ],
   providers: [ProfitcalculatorService],
   bootstrap: [AppComponent]
